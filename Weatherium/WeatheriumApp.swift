e4283301a43534da8b14fc6d558624c43f792fe6
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-08-20 11:34:09 +0300

    created project

---
+++ Weatherium/WeatheriumApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheriumApp.swift
+//  Weatherium
+//
+//  Created by Yurii Samoienko on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatheriumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
