291e02495abd502207a1a0dcddf56626537c6a1e
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-08-22 09:44:01 +0300

    wether data is loaded asynchronisly

--- Weatherium/Models/WeatherViewModel.swift
+++ Weatherium/Models/WeatherViewModel.swift
@@ -28,9 +28,7 @@ import Combine
         
         subscriptions = [
             citiesViewModel.$cities.sink(receiveValue: { (cities: [CityData]) in
-                Task {
-                    await self.updateCitiesWeather(for: cities)
-                }
+                self.updateCitiesWeather(for: cities)
             }),
         ]
 
@@ -38,62 +36,19 @@ import Combine
     
     // MARK: Private Functions
     
-    private func updateCitiesWeather(for cities: [CityData]) async {
-//        let myDict = [1,2,3].reduce(into: [Int: String]()) {
-//            $0[$1.position] = $1.name
-//        }
-        
-//        let weathers: [CityData: WeatherData] = await cities.reduce(into: [:]) { city, _ in
-//
-//        }
+    private func updateCitiesWeather(for cities: [CityData]) {
         for city in cities {
-//            var coordinate: CLLocationCoordinate2D? = citiesCoordinate[city]
-//            if coordinate == nil {
-//                var address = city.name
-//                if let country = city.country {
-//                    address += ", \(country)"
-//                }
-//                address += " \(city.id)"
-//                do {
-//                    let placemarks: [CLPlacemark] = try await CLGeocoder().geocodeAddressString(address)
-//                    coordinate = placemarks.first(where: { $0.location != nil })?.location?.coordinate
-//                } catch {
-//                    print("failed geocode address '\(address)' with error: \(error.localizedDescription)")
-//                }
-//            }
-//            guard let coordinate = await self.getCoordinateOf(city: city) else {
-//                continue
-//            }
-//
-//            let weatherResponse: WeatherRequestResponse
-//            do {
-//                weatherResponse = try await self.networkRequestService.getDataFrom(
-//                    endpoint: .weather(
-//                        latitude: coordinate.latitude,
-//                        longitude: coordinate.longitude
-//                    )
-//                )
-//            } catch {
-//                print("failed decode WeatherResponse with error:", error.localizedDescription)
-//                return
-//            }
-//            let weatherInCity = WeatherData(
-//                weatherDescription: weatherResponse.getDescription() ?? "",
-//                temperature: WeatherTemperature(
-//                    high: Int(weatherResponse.getMaxTemperature()),
-//                    low: Int(weatherResponse.getMinTemperature())
-//                ),
-//                icon: weatherResponse.getIconKey() ?? ""
-//            )
-            let weatherInCity: WeatherData
-            do {
-                weatherInCity = try await self.getWeatherIn(city: city)
-            } catch {
-                print("failed decode WeatherResponse with error:", error.localizedDescription)
-                continue
+            Task {
+                let weatherInCity: WeatherData
+                do {
+                    weatherInCity = try await self.getWeatherIn(city: city)
+                } catch {
+                    print("failed decode WeatherResponse with error:", error.localizedDescription)
+                    return
+                }
+                //print("weatherResponse: \n", weatherInCity) // uncomment for debugging
+                self.citiesWeather[city] = weatherInCity
             }
-            //print("weatherResponse: \n", weatherInCity) // uncomment for debugging
-            self.citiesWeather[city] = weatherInCity
         }
     }
     
