2c6998946a7c62d31c60c13c0ad2fc0a5ef58e75
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-08-22 08:56:17 +0300

    refactored

--- Weatherium/Models/WeatherViewModel.swift
+++ Weatherium/Models/WeatherViewModel.swift
@@ -12,12 +12,15 @@ import Combine
 @MainActor class WeatherViewModel: ObservableObject {
     
     @Published var citiesWeather: [CityData: WeatherData] = [:]
+    @Published var citiesForecast: [CityData: ForecastData] = [:]
 
     // MARK: Private Properties
     
     private var citiesViewModel = CitiesViewModel()
     private var subscriptions = Set<AnyCancellable>()
     private let networkRequestService: NetworkRequestServicePl = NetworkRequestService() //TODO @Injected
+    
+    private var citiesCoordinate: [CityData: CLLocationCoordinate2D] = [:]
     
     // MARK: Public Functions
     
@@ -36,55 +39,103 @@ import Combine
     // MARK: Private Functions
     
     private func updateCitiesWeather(for cities: [CityData]) async {
+//        let myDict = [1,2,3].reduce(into: [Int: String]()) {
+//            $0[$1.position] = $1.name
+//        }
+        
+//        let weathers: [CityData: WeatherData] = await cities.reduce(into: [:]) { city, _ in
+//
+//        }
         for city in cities {
-            var coordinate: CLLocationCoordinate2D?
+//            var coordinate: CLLocationCoordinate2D? = citiesCoordinate[city]
+//            if coordinate == nil {
+//                var address = city.name
+//                if let country = city.country {
+//                    address += ", \(country)"
+//                }
+//                address += " \(city.id)"
+//                do {
+//                    let placemarks: [CLPlacemark] = try await CLGeocoder().geocodeAddressString(address)
+//                    coordinate = placemarks.first(where: { $0.location != nil })?.location?.coordinate
+//                } catch {
+//                    print("failed geocode address '\(address)' with error: \(error.localizedDescription)")
+//                }
+//            }
+//            guard let coordinate = await self.getCoordinateOf(city: city) else {
+//                continue
+//            }
+//
+//            let weatherResponse: WeatherRequestResponse
+//            do {
+//                weatherResponse = try await self.networkRequestService.getDataFrom(
+//                    endpoint: .weather(
+//                        latitude: coordinate.latitude,
+//                        longitude: coordinate.longitude
+//                    )
+//                )
+//            } catch {
+//                print("failed decode WeatherResponse with error:", error.localizedDescription)
+//                return
+//            }
+//            let weatherInCity = WeatherData(
+//                weatherDescription: weatherResponse.getDescription() ?? "",
+//                temperature: WeatherTemperature(
+//                    high: Int(weatherResponse.getMaxTemperature()),
+//                    low: Int(weatherResponse.getMinTemperature())
+//                ),
+//                icon: weatherResponse.getIconKey() ?? ""
+//            )
+            let weatherInCity: WeatherData
+            do {
+                weatherInCity = try await self.getWeatherIn(city: city)
+            } catch {
+                print("failed decode WeatherResponse with error:", error.localizedDescription)
+                continue
+            }
+            //print("weatherResponse: \n", weatherInCity) // uncomment for debugging
+            self.citiesWeather[city] = weatherInCity
+        }
+    }
+    
+    private func getWeatherIn(city: CityData) async throws -> WeatherData {
+        guard let coordinate = await self.getCoordinateOf(city: city) else {
+            throw CError(message: "coordinates not found for city: \(city.name)")
+        }
+        
+        let weatherResponse: WeatherRequestResponse
+        weatherResponse = try await self.networkRequestService.getDataFrom(
+            endpoint: .weather(
+                latitude: coordinate.latitude,
+                longitude: coordinate.longitude
+            )
+        )
+        let result = WeatherData(
+            weatherDescription: weatherResponse.getDescription() ?? "",
+            temperature: WeatherTemperature(
+                high: Int(weatherResponse.getMaxTemperature()),
+                low: Int(weatherResponse.getMinTemperature())
+            ),
+            icon: weatherResponse.getIconKey() ?? ""
+        )
+        return result
+    }
+    
+    private func getCoordinateOf(city: CityData) async -> CLLocationCoordinate2D? {
+        var coordinate: CLLocationCoordinate2D? = citiesCoordinate[city]
+        if coordinate == nil {
             var address = city.name
             if let country = city.country {
                 address += ", \(country)"
             }
             address += " \(city.id)"
+            
             do {
                 let placemarks: [CLPlacemark] = try await CLGeocoder().geocodeAddressString(address)
                 coordinate = placemarks.first(where: { $0.location != nil })?.location?.coordinate
             } catch {
                 print("failed geocode address '\(address)' with error: \(error.localizedDescription)")
             }
-            guard let coordinate = coordinate else {
-                return
-            }
-            
-            let weatherResponse: WeatherRequestResponse
-            do {
-                weatherResponse = try await self.networkRequestService.getDataFrom(
-                    endpoint: .weather(
-                        latitude: coordinate.latitude,
-                        longitude: coordinate.longitude
-                    )
-                )
-            } catch {
-                print("failed decode WeatherResponse with error:", error.localizedDescription)
-                return
-            }
-            let weatherInCity = WeatherData(
-                weatherDescription: weatherResponse.getDescription() ?? "",
-                temperature: WeatherTemperature(
-                    high: Int(weatherResponse.getMaxTemperature()),
-                    low: Int(weatherResponse.getMinTemperature())
-                ),
-                icon: weatherResponse.getIconKey() ?? ""
-            )
-            //print("weatherResponse: \n", weatherInCity) // uncomment for debugging
-            self.citiesWeather[city] = weatherInCity
         }
+        return coordinate
     }
-    
-    private func getWeather(for coorditante: CLLocationCoordinate2D) async throws -> Int {
-        try await withCheckedThrowingContinuation { continuation in
-            let onError: (String) -> Void = { message in
-                continuation.resume(throwing: CError(message: message))
-            }
-            continuation.resume(returning: 0)
-        }
-    }
-    
 }
